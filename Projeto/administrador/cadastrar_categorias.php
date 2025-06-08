9e71f87c7da394c23a4fd999f36de921e99ed0a2
ElisFliess <102757856+Elisfliess@users.noreply.github.com>
2025-06-08 01:51:15 -0300

    Update cadastrar_categorias.php
    style tela cadastrar categorias

--- Projeto/administrador/cadastrar_categorias.php
+++ Projeto/administrador/cadastrar_categorias.php
@@ -51,7 +51,122 @@ if ($stmt->fetchColumn() > 0) {
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Categoria</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: #f3f3f3;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: Arial, sans-serif;
+        }
+
+        .cubo {
+            background-color: white;
+            border: 2px solid #9E7FAF;
+            border-radius: 15px;
+            padding: 30px 40px;
+            width: 400px;
+            box-shadow: 0px 0px 20px rgba(158, 127, 175, 0.3);
+        }
+
+        .cubo h2 {
+            text-align: center;
+            color: #9E7FAF;
+        }
+
+        label {
+            font-weight: bold;
+            display: block;
+            margin-top: 15px;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            margin-top: 5px;
+            border-radius: 8px;
+            border: 1px solid #ccc;
+        }
+
+        input[type="checkbox"] {
+            margin-top: 10px;
+        }
+
+  
+    .cubo button {
+    width: 100%;
+    background-color: #9E7FAF;
+    color: white;
+    border: none;
+    padding: 12px;
+    margin-top: 20px;
+    border-radius: 8px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+
+
+        button:hover {
+            background-color: #7e5f90;
+        }
+
+        a {
+            display: inline-block;
+            margin-top: 10px;
+            text-decoration: none;
+            color: #9E7FAF;
+            font-weight: bold;
+            transition: color 0.3s;
+        }
+        a:hover {
+            color: #7f6390;
+        }
+        p {
+            margin: 0 0 10px;
+            
+        }
+        .links {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .links a {
+            color: #9E7FAF;
+            text-decoration: none;
+            display: inline-block;
+            margin: 5px 0;
+        }
+
+        .links a:hover {
+            text-decoration: underline;
+        }
+
+        .email-group {
+    position: relative;
+    display: flex;
+    align-items: center;
+}
+
+.email-group input[type="text"] {
+    padding-right: 110px;
+    flex: 1;
+}
+
+.email-domain {
+    position: absolute;
+    right: 10px;
+    color: #555;
+    pointer-events: none;
+    font-size: 14px;
+}
+    </style>
 
 </head>
 <body>
